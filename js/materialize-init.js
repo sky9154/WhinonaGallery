bda29412e94faef092aa075658d67afea54f924e
oF <sky0932039612@gmail.com>
2021-07-30 03:26:45 +0800

    整理js

--- js/materialize-init.js
+++ js/materialize-init.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-  // Init Scrollspy
-  $('.scrollspy').scrollSpy()
-  // Init slider
-  $('.slider').slider({
-    indicators: false,
-  })
-  // Init Sidenav
-  $('.sidenav').sidenav({
-    edge: 'right',
-    preventScrolling: false
-  });
+    // Init Scrollspy
+    $('.scrollspy').scrollSpy()
+    // Init slider
+    $('.slider').slider({
+        indicators: false,
+    })
+    // Init Sidenav
+    $('.sidenav').sidenav({
+        edge: 'right',
+        preventScrolling: false
+    });
 });
